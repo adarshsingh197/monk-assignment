52e37b2b21a005680a46aca402736e1f9e609964
adarshsingh197 <singhadarsh1797@gmail.com>
2026-02-11 21:23:14 +0530

    created initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ProductProvider } from './context/ProductProvider';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <ProductProvider>
+
+    </ProductProvider>
+    </>
+  )
+}
+
+export default App
